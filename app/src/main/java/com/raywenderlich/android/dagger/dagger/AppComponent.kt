596634eb3acef966a750a761f04e89995d2db56a
kik <henriquezanferrari@gmail.com>
2020-05-04 23:06:15 -0300

    Injeção da HomepageActivity

--- app/src/main/java/com/raywenderlich/android/dagger/dagger/AppComponent.kt
+++ app/src/main/java/com/raywenderlich/android/dagger/dagger/AppComponent.kt
@@ -1,9 +1,14 @@
 package com.raywenderlich.android.dagger.dagger
 
+import com.raywenderlich.android.dagger.ui.homepage.HomepageActivity
 import dagger.Component
 import javax.inject.Singleton
 
 @Singleton
 // The component is used to connect objects to their dependencies,
 @Component(modules = [AppModule::class])
-interface AppComponent
+interface AppComponent {
+
+    // The HomepageActivity class will require injection from AppComponent.
+    fun inject(target: HomepageActivity)
+}
